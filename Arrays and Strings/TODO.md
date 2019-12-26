41a59c1467620baf5bb5401e39ee339c20c54fab
devprabal <ghjxvnjd@gmail.com>
2019-12-26 22:19:52 +0530

    1.7 Rotate Matrix

--- Arrays and Strings/TODO.md
+++ Arrays and Strings/TODO.md
@@ -1,10 +1,12 @@
 
-- remove all the binaries from the commit
+- [x] remove all the binaries from the commit
 
-- write a cmake to compile all files and generate all corresponding binaries with appropriate names in `bin/` dir
+- [ ] write a cmake to compile all files and generate all corresponding binaries with appropriate names in `bin/` dir
 
-- can all the prior commits be renamed to something like `chapter_number dot question_number commit_message`? 
+- [ ] can all the prior commits be renamed to something like `chapter_number dot question_number commit_message`? 
 example : "1.4 Palindrome permutation"
 
 >(if so, one can make a gerrit like system where `chapter_name.question_number` can become a hyperlink)
+
+- [ ] pick individual files and do the changes enlisted in the paper, see if those commits can be changed?
 
---
+++ Arrays and Strings/rotateMatrix.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std;
+// TODO: is this correct method for nxn matrix to be passed or not?
+void printMatrix(int **mat, int rows, int cols) {
+  for (int i = 0; i < rows; i++) {
+    for (int j = 0; j < cols; j++)
+      cout << mat[i][j] << " ";
+    cout << '\n';
+  }
+  cout << '\n';
+}
+// TODO: this can be made as a utility function in a separate file and included
+// in a header? How to do that? Library?
+void rotateMatrix(int **mat, int n) {
+  for (int layer = 0; layer < n / 2; layer++) {
+    for (int row = layer; row < n - layer - 1; row++) {
+      // save top
+      int temp = mat[layer][row];
+      // top -> left
+      mat[layer][row] = mat[n - layer - 1 - row][layer];
+      // left -> bottom
+      mat[n - layer - 1 - row][layer] = mat[n - layer - 1][n - layer - 1 - row];
+      // bottom -> right
+      mat[n - layer - 1][n - layer - 1 - row] = mat[layer + row][n - 1 - layer];
+      // right -> top
+      mat[layer + row][n - 1 - layer] = temp;
+    }
+  }
+}
+int main(int argc, char const *argv[]) {
+  int **mat = nullptr;
+  int rows = 3, cols = 3;
+  // cin >> rows >> cols;
+  int n;
+  if (rows != cols)
+    // to know the exit status returned by the previous program in the terminal
+    // type: echo $?
+    exit(EXIT_FAILURE);
+  else
+    n = rows;
+  mat = new int *[rows];
+  for (int i = 0; i < rows; i++) {
+    mat[i] = new int[cols];
+  }
+  mat[0][0] = 1;
+  mat[0][1] = 2;
+  mat[0][2] = 3;
+  mat[1][0] = 4;
+  mat[1][1] = 5;
+  mat[1][2] = 6;
+  mat[2][0] = 7;
+  mat[2][1] = 8;
+  mat[2][2] = 9;
+
+  // int mat[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
+  // no. of rows = sizeof mat / sizeof mat[0];
+  // no. of cols = sizeof mat[0] / sizeof int
+  printMatrix(mat, rows, cols);
+  rotateMatrix(mat, n);
+  printMatrix(mat, rows, cols);
+  return 0;
+}
